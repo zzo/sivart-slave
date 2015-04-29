8c374b8dc5f9b6add1996eeb5a798bf3fd2ae7b8
Mark Ethan Trostler <mark@zzo.com>
2015-04-29 00:32:34 -0700

    fixes

--- deleteInstance.js
+++ deleteInstance.js
@@ -1,7 +1,8 @@
 var Instance = require('sivart-GCE/Instance');
 var projectId = 'focal-inquiry-92622';
 var zone = 'us-central1-a';
-var hostname = require('os').hostname();
+var instanceName = process.argv[2];
+// should be able to get this stuff automatically!
 var sivart_slave = new Instance(projectId, zone, instanceName);
 sivart_slave.delete(function() {
   // bye bye!
