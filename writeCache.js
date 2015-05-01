a6cd61c5c4ff66dfd8c49b0e3fc2565f012367fa
Mark Ethan Trostler <mark@zzo.com>
2015-05-01 06:50:03 -0700

    eslint fixes

--- writeCache.js
+++ writeCache.js
@@ -1,12 +1,13 @@
+'use strict';
+
 var fs = require('fs');
 var path = require('path');
 var Auth = require('sivart-GCE/Auth');
 var os = require('os');
-var targz = require('tar.gz');
 var printf = require('util').format;
 var hash = require('crypto').createHash('md5');
 
-// Get info 
+// Get info
 var cacheDir = process.argv[2];
 var repoName = process.argv[3];
 var branch = process.argv[4];
@@ -22,47 +23,59 @@ var baseName = path.basename(tmpFile);
 var safeRepo = repoName.replace(/\//g, '-');
 var bucketname = ['sivart', safeRepo, branch].join('-');
 
+function getHash(file, cb) {
+  var stream = fs.createReadStream(file);
+
+  stream.on('data', function (data) {
+    hash.update(data, 'utf8');
+  });
+
+  stream.on('end', function () {
+    cb(hash.digest('base64'));
+  });
+}
+
 console.log(printf('Upload cache directory "%s" on branch "%s" (maybe)', cacheDir, branch));
-storage.createBucket(bucketname, function(err, bucket) {
-  if (err) {
+storage.createBucket(bucketname, function(cberr, bucket) {
+  if (cberr) {
     bucket = storage.bucket(bucketname);
-  } 
+  }
 
   var exec = require('child_process').exec;
   exec(printf('tar caf %s %s', tmpFile, cacheDir), { cwd: process.cwd() },
-    function(err, stdout, stderr) {
-      if (err) {
-        console.log("error compressing:");
-        console.log(err);
+    function(exerr) { // , stdout, stderr) {
+      if (exerr) {
+        console.log('error compressing:');
+        console.log(exerr);
       } else {
       // Get current file & see if we wanna re-save it
       //    if hash values are different
       getHash(tmpFile, function(hashValue) {
-        bucket.getFiles({prefix: baseName}, function handleResults(err, files) {
-          if (err || !files[0]) {
+        bucket.getFiles({prefix: baseName}, function handleResults(gferr, files) {
+          if (gferr || !files[0]) {
             files[0] = bucket.file(baseName);
-            console.log(err || 'cache file does not exist: ' + baseName);
+            console.log(gferr || 'cache file does not exist: ' + baseName);
           }
           if (files[0]) {
             console.log(files[0].metadata);
             console.log(hashValue);
             var stat = fs.lstatSync(tmpFile);
             console.log(stat);
-            if (files[0].metadata.md5Hash != hashValue) {
+            if (files[0].metadata.md5Hash !== hashValue) {
     files[0].download(
-      { destination: tmpFile + '.orig' }, 
+      { destination: tmpFile + '.orig' },
       function(err) {
         if (err) {
-          console.log('Error getting cache directory: ' + tmpPath);
+          console.log('Error getting cache directory: ' + tmpFile);
         } else {
 
               // ship it
               console.log(printf('Directory changed - uploading...'));
               bucket.upload(tmpFile, { destination: baseName },
-                function(err, file) {
-                  if (err) {
-                    consoloe.log(printf('Error updating cached directory'));
-                    console.log(err);
+                function(uperr) { // , file) {
+                  if (uperr) {
+                    console.log(printf('Error updating cached directory'));
+                    console.log(uperr);
                   } else {
                     console.log(printf('Successfully saved directory'));
                   }
@@ -101,7 +114,7 @@ storage.createBucket(bucketname, function(err, bucket) {
             console.log(stat);
             if (files[0].metadata.md5Hash != hashValue) {
     files[0].download(
-      { destination: tmpFile + '.orig' }, 
+      { destination: tmpFile + '.orig' },
       function(err) {
         if (err) {
           console.log('Error getting cache directory: ' + tmpPath);
@@ -131,14 +144,4 @@ storage.createBucket(bucketname, function(err, bucket) {
 });
   */
 
-function getHash(file, cb) {
-  var stream = fs.createReadStream(file);
 
-  stream.on('data', function (data) {
-    hash.update(data, 'utf8')
-  })
-
-  stream.on('end', function () {
-    cb(hash.digest('base64'));
-  })
-}
