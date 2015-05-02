643f7f66d6a8edf1a7ca36a99ee4afbd2dab6720
Mark Ethan Trostler <mark@zzo.com>
2015-05-02 13:18:03 -0700

    sivart_timeout

--- CreateScript.js
+++ CreateScript.js
@@ -206,7 +206,7 @@ CreateScript.prototype.getScripts = function(cb) {
       scripts.forEach(function(script) {
         var template = fs.readFileSync(path.join(__dirname, 'startup.sh.template'), 'utf8');
         var startupScript = template.replace('USER_SCRIPT', script.script.join("\n"));
-        startupScript = startupScript.replace('TIMEOUT', this.timeout);
+        startupScript = startupScript.replace('SIVART_TIMEOUT', me.timeout);
         done.push({script: startupScript, metadata: script.metadata});
       });
       cb(null, done);
