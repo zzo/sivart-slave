61e15bc12f09a0bbea6613096a864e3b1dec4452
Mark Ethan Trostler <mark@zzo.com>
2015-05-04 14:12:39 -0700

    better instance names

--- Project.js
+++ Project.js
@@ -63,7 +63,8 @@ Project.prototype.createScripts = function(cb) {
 
 Project.prototype.createSlave = function(script, cb) {
   var me = this;
-  var instanceName = uuid.v1();
+  var safeName = this.repoName.replace(/[^0-9a-zA-Z-]/g, '-');
+  var instanceName = [new Date().getTime(), uuid.v4(), safeName].join('-').slice(0,63);
   var data = JSON.parse(fs.readFileSync(this.slaveFile));
   data.name = instanceName;
   data.disks[0].deviceName = 'slave-disk';
