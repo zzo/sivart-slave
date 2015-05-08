92648d9e425aa1ab1bdc867685e2d08d1688eb49
Mark Ethan Trostler <mark@zzo.com>
2015-05-08 08:33:26 -0700

    fixup savefiles

--- saveLogs.js
+++ saveLogs.js
@@ -2,11 +2,7 @@
 
 var fs = require('fs');
 var path = require('path');
-var hostname = require('os').hostname();
-var Auth = require('sivart-GCE/Auth');
-var gcloud = require('gcloud');
 var WriteData = require('sivart-data/WriteBuildData');
-var storage = gcloud.storage(Auth);
 
 // Get info
 var logDir = process.env.SIVART_BASE_LOG_DIR;
@@ -14,25 +10,24 @@ var repoName = process.env.TRAVIS_REPO_SLUG;
 var branch = process.env.TRAVIS_BRANCH;
 var buildId = process.env.SIVART_BUILD_ID;
 var buildNumber = process.env.SIVART_BUILD_NUMBER;
-
 var writeData = new WriteData(repoName);
-var bucketname = writeData.getBucketName();
 
+// Save environment (for internal debugging)
 fs.writeFileSync(path.join(logDir, 'environment.json'), JSON.stringify(process.env));
 
-// Save files
-var basepath = path.join(branch, String(buildId), String(buildNumber));
-
-// Store files
-storage.createBucket(bucketname, function(err, bucket) {
-  if (err) {
-    bucket = storage.bucket(hostname);
-  }
-  var files = fs.readdirSync(logDir);
-  files.forEach(function(file) {
-    fs.createReadStream(path.join(logDir, file)).pipe(bucket.file(path.join(basepath, file)).createWriteStream());
-  });
-  fs.createReadStream('/tmp/user-script.sh').pipe(bucket.file(path.join(basepath, 'user-script.sh')).createWriteStream());
-  fs.createReadStream('/tmp/user-script.log').pipe(bucket.file(path.join(basepath, 'user-script.log')).createWriteStream());
-  fs.createReadStream('/var/log/startupscript.log').pipe(bucket.file(path.join(basepath, 'startupscript.log')).createWriteStream());
+// Save all files in logDir...
+var files = fs.readdirSync(logDir).map(function(file) {
+  return path.join(logDir, file);
 });
+
+// ..and save these files too
+files.push(
+  '/tmp/user-script.sh',
+  '/tmp/user-script.log',
+  '/var/log/startupscript.log'
+);
+
+// Save files to here within bucket
+//  branch name / build id / build number
+var basepath = path.join(branch, String(buildId), String(buildNumber));
+writeData.writeFilesToBucket(basepath, files, function() {});
