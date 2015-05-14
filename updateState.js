ac44829a4656fa12c0f179da9e9c5fd4737c2dd3
Mark Ethan Trostler <mark@zzo.com>
2015-05-14 09:16:23 -0700

    use correct function name

--- updateState.js
+++ updateState.js
@@ -8,7 +8,7 @@ var datastore = new Datastore(process.env.TRAVIS_REPO_SLUG);
 var buildId = process.env.SIVART_BUILD_ID;
 var buildNumber = process.env.SIVART_BUILD_NUMBER;
 
-datastore.updateState(buildId, buildNumber, newState, function(err) {
+datastore.updateRunState(buildId, buildNumber, newState, function(err) {
   if (err) {
     console.log('Update build state error:');
     console.log(err);
